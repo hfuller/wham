91d2a92675394d81deec2909fd51b1ddab17aa07
Hunter Fuller <hfuller@pixilic.com>
2016-03-16 23:55:28 -0500

    redirect / to web ui

--- whamservice.py
+++ whamservice.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Hello World!"
+	return redirect("/static/index.html")
 
 @app.route('/zones/')
 def get_zones():
-    return 'zones'
+	return 'zones'
 
 @app.route('/sources/')
 def get_sources():
-    return 'sources'
+	return 'sources'
     
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0')
+	app.debug = True
+	app.run(host='0.0.0.0')
 
 
 
