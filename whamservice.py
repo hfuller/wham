cf8d60757b92275a776a7280f0c2aba13f3ae861
Hunter Fuller <hfuller@pixilic.com>
2016-06-25 01:08:01 -0500

    config reading and shit

--- whamservice.py
+++ whamservice.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python
 
 from flask import Flask, redirect
+import ConfigParser, json
+
 app = Flask(__name__)
+inputs = {}
+outputs = {}
+
 
 @app.route('/')
 def homepage():
 	return redirect("/static/index.html")
 
-@app.route('/zones/')
-def get_zones():
-	return 'zones'
+@app.route('/inputs/')
+def get_inputs():
+	return 'x'
 
-@app.route('/sources/')
-def get_sources():
-	return 'sources'
-    
+@app.route('/outputs/')
+def get_outputs():
+	return 'y'
+
+
+def load_config():
+        global inputs, outputs
+        input_cp = ConfigParser.ConfigParser()
+        output_cp = ConfigParser.ConfigParser()
+        input_cp.read("input.ini")
+        output_cp.read("output.ini")
+        inputs = config_parser_to_json(input_cp)
+        outputs = config_parser_to_json(output_cp)
+
+def config_parser_to_json(cp):
+        data = {}
+        for s in cp.sections():
+                s = s
+                data[int(s)] = {}
+                for o in cp.options(s):
+                        data[int(s)][o] = cp.get(s,o)
+        return data
+
 if __name__ == '__main__':
 	app.debug = True
+
+        load_config()
+        if app.debug:
+                print "INPUTS: " + json.dumps(inputs)
+                print "OUTPUTS: " + json.dumps(outputs)
+        
 	app.run(host='0.0.0.0')
 
 
