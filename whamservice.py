1842bbd339ee4330ad47ac2c4a820a38707753d4
Hunter Fuller <hfuller@pixilic.com>
2016-06-26 14:00:54 -0500

    Config is json instead of ConfigParser

--- whamservice.py
+++ whamservice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from flask import Flask, redirect
-import ConfigParser, json
+import json
 
 app = Flask(__name__)
 inputs = {}
@@ -21,28 +21,12 @@ def get_outputs():
 	return 'y'
 
 
-def load_config():
-        global inputs, outputs
-        input_cp = ConfigParser.ConfigParser()
-        output_cp = ConfigParser.ConfigParser()
-        input_cp.read("input.ini")
-        output_cp.read("output.ini")
-        inputs = config_parser_to_json(input_cp)
-        outputs = config_parser_to_json(output_cp)
-
-def config_parser_to_json(cp):
-        data = {}
-        for s in cp.sections():
-                s = s
-                data[int(s)] = {}
-                for o in cp.options(s):
-                        data[int(s)][o] = cp.get(s,o)
-        return data
-
 if __name__ == '__main__':
 	app.debug = True
 
-        load_config()
+	with open('inputs.json') as f: inputs = json.loads(f.read())
+	with open('outputs.json') as f: outputs = json.loads(f.read())
+
         if app.debug:
                 print "INPUTS: " + json.dumps(inputs)
                 print "OUTPUTS: " + json.dumps(outputs)
