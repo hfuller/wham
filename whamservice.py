24bbcc7d56f09f8e2947093d24197605fae52725
Hunter Fuller <hfuller@pixilic.com>
2017-01-05 23:45:59 -0600

    use the controller luke

--- whamservice.py
+++ whamservice.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 from flask import Flask, redirect, request
-import json
+import jsonpickle
+from s128p import S128P
 
 app = Flask(__name__)
-inputs = None
-outputs = None
+controller = None
 
 
 @app.route('/')
@@ -14,53 +14,55 @@ def homepage():
 
 @app.route('/inputs/')
 def get_inputs():
-	return json.dumps(inputs.values())
+	return jsonpickle.encode(list(input.id for input in controller.inputs), unpicklable=False)
 @app.route('/inputs/<int:id>', methods=['GET'])
 def get_input(id):
-	return json.dumps(inputs[int(id)])
+	return jsonpickle.encode(controller.inputs[int(id)], unpicklable=False)
 
 #TODO: remove duplication here.
 @app.route('/outputs/')
 def get_outputs():
-	return json.dumps(outputs.values())
+	return jsonpickle.encode(list(output.id for output in controller.outputs), unpicklable=False)
 @app.route('/outputs/<int:id>', methods=['GET'])
 def get_output(id):
-	return json.dumps(outputs[int(id)])
+	return jsonpickle.encode(controller.outputs[int(id)], unpicklable=False)
 @app.route('/outputs/<int:id>', methods=['PUT'])
 def put_output(id):
 	print request.form
 
 	try:
-		outputs[id]['volume'] = int(request.form['volume'])
+		controller.set_volume(id, int(request.form['volume']))
 	except KeyError:
 		print "not updating volume"
 	try:
-		outputs[id]['input'] = json.loads(request.form['input']) #handle ints or 'null'
+		controller.set_input(id, jsonpickle.decode(request.form['input'])) #handle ints or 'null'
 	except KeyError:
 		print "not updating input" 
 
-	print outputs[id]
+	print controller.outputs[id]
 	return ('',200)
 
 if __name__ == '__main__':
 	app.debug = True
 
-	with open('inputs.json') as f: inputs = json.loads(f.read())
-	with open('outputs.json') as f: outputs = json.loads(f.read())
+	#with open('inputs.json') as f: inputs = json.loads(f.read())
+	#with open('outputs.json') as f: outputs = json.loads(f.read())
 
 	#convert them to dicts
-	inputs = dict(zip([x['id'] for x in inputs], inputs))
-	outputs = dict(zip([x['id'] for x in outputs], outputs))
+	#inputs = dict(zip([x['id'] for x in inputs], inputs))
+	#outputs = dict(zip([x['id'] for x in outputs], outputs))
 	
 	#prep outputs reasonably
-	print outputs
-	for id in outputs:
-		outputs[id]['volume'] = 50
-		outputs[id]['input'] = None
+	#print outputs
+	#for id in outputs:
+	#	outputs[id]['volume'] = 50
+	#	outputs[id]['input'] = None
+
+        controller = S128P()
 
         if app.debug:
-                print "INPUTS: " + json.dumps(inputs)
-                print "OUTPUTS: " + json.dumps(outputs)
+                print "INPUTS: " + jsonpickle.encode(controller.inputs, unpicklable=False)
+                print "OUTPUTS: " + jsonpickle.encode(controller.outputs, unpicklable=False)
         
 	app.run(host='0.0.0.0')
 
